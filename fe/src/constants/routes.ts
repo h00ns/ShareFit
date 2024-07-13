a82f5a4d935dff7cb0d2a780a53db96c974ff1d5
h00ns <jihoon.hwang@imweb.me>
2024-07-13 12:54:10 +0900

    [FE] HOTFIX : env 누락 수정

--- fe/src/constants/routes.ts
+++ fe/src/constants/routes.ts
@@ -1,1 +1,1 @@
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL}&response_type=code`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
